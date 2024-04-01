19c9882155386dc6200a26c05d43456d70a166b6
zeljko <zeljko.kljajic090503@gmail.com>
2024-04-01 20:26:28 +0200

    fertig???

---
+++ Src2/TicTacToe.java
@@ -0,0 +1,129 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        // Gerüst aufstellen
+        char[][] playBoard = new char[][] {
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}
+        };
+
+        // Namen der Spieler erhalten
+        printText();
+
+        // Spielfeld anzeigen
+        printPlayBoard(playBoard);
+
+        // Unendliche Schleife für das Spiel
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+
+            // Spieler X
+            System.out.println("Enter position of X (1 - 9): ");
+            int xPos = scanner.nextInt();
+            xPlay(playBoard, xPos, "X");
+
+            // Spieler O
+            System.out.println("Enter position of O (1 - 9): ");
+            int oPos = scanner.nextInt();
+            oPlay(playBoard, oPos, "O");
+        }
+    }
+
+    public static void xPlay(char[][] playBoard, int xPos, String player) {
+        switch (xPos) {
+            case 1:
+                playBoard[0][0] = 'X';
+                break;
+            case 2:
+                playBoard[0][2] = 'X';
+                break;
+            case 3:
+                playBoard[0][4] = 'X';
+                break;
+            case 4:
+                playBoard[2][0] = 'X';
+                break;
+            case 5:
+                playBoard[2][2] = 'X';
+                break;
+            case 6:
+                playBoard[2][4] = 'X';
+                break;
+            case 7:
+                playBoard[4][0] = 'X';
+                break;
+            case 8:
+                playBoard[4][2] = 'X';
+                break;
+            case 9:
+                playBoard[4][4] = 'X';
+                break;
+            default:
+                break;
+        }
+        printPlayBoard(playBoard);
+    }
+
+    public static void oPlay(char[][] playBoard, int oPos, String player) {
+        switch (oPos) {
+            case 1:
+                playBoard[0][0] = 'O';
+                break;
+            case 2:
+                playBoard[0][2] = 'O';
+                break;
+            case 3:
+                playBoard[0][4] = 'O';
+                break;
+            case 4:
+                playBoard[2][0] = 'O';
+                break;
+            case 5:
+                playBoard[2][2] = 'O';
+                break;
+            case 6:
+                playBoard[2][4] = 'O';
+                break;
+            case 7:
+                playBoard[4][0] = 'O';
+                break;
+            case 8:
+                playBoard[4][2] = 'O';
+                break;
+            case 9:
+                playBoard[4][4] = 'O';
+                break;
+            default:
+                break;
+        }
+        printPlayBoard(playBoard);
+    }
+
+    public static void printPlayBoard(char[][] playBoard) {
+        // Durch das Gerüst iterieren
+        for (char[] colom : playBoard) {
+            for (char rows : colom) {
+                System.out.print(rows);
+            }
+            System.out.println();
+        }
+
+    }
+    public static void printText() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Name des Spielers für X: ");
+        String nameX = scanner.nextLine();
+
+        System.out.println("Name des Spielers für O: ");
+        String nameO = scanner.nextLine();
+        System.out.println("Das Spiel beginnt!");
+    }
+    public static void checkWinner() {
+
+    }
+}
